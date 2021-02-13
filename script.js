c66279fcaf2c34215abd3857a670ec37407110b9
Marinarium <schwmk@gmail.com>
2021-02-14 02:42:54 +0300

    fix: fix bug with UTC format

--- script.js
+++ script.js
@@ -4,55 +4,55 @@ const holidays = {
     valentinesDay: {
         name: 'Valentine\'s Day',
         dateTitle: 'the 14 february',
-        date: `${currentYear}-02-14`,
+        date: `${currentYear}-02-14 00:00:00`,
         image: 'valentines-day.jpg'
     },
     womensDay: {
         name: 'Women\'s Day',
         dateTitle: 'the 8 march',
-        date: `${currentYear}-03-08`,
+        date: `${currentYear}-03-08 00:00:00`,
         image: 'womens-day.jpg'
     },
     saintPatricksDay: {
         name: 'Saint Patrick\'s Day',
         dateTitle: 'the 17 march',
-        date: `${currentYear}-03-17`,
+        date: `${currentYear}-03-17 00:00:00`,
         image: 'saint-patrick.jpg'
     },
     victoryDay : {
         name: 'Victory Day ',
         dateTitle: 'the 9 may',
-        date: `${currentYear}-05-09`,
+        date: `${currentYear}-05-09 00:00:00`,
         image: 'victory-day.jpg'
     },
     kupalaNight : {
         name: 'Kupala Night ',
         dateTitle: 'the 24 June',
-        date: `${currentYear}-06-24`,
+        date: `${currentYear}-06-24 00:00:00`,
         image: 'kupala-night.jpg'
     },
     watermelonDay : {
         name: 'Watermelon Day',
         dateTitle: 'the 3 August',
-        date: `${currentYear}-08-03`,
+        date: `${currentYear}-08-03 00:00:00`,
         image: 'watermelon-day.jpg'
     },
     knowledgeDay : {
         name: 'Knowledge Day',
         dateTitle: 'the 1 September',
-        date: `${currentYear}-09-01`,
+        date: `${currentYear}-09-01 00:00:00`,
         image: 'knowledge-day.jpg'
     },
     halloween : {
         name: 'Halloween',
         dateTitle: 'the 31 October',
-        date: `${currentYear}-10-31`,
+        date: `${currentYear}-10-31 00:00:00`,
         image: 'halloween.jpg'
     },
     newYear: {
         name: 'New Year',
         dateTitle: 'the 1 january',
-        date: `${currentYear + 1}-01-01`,
+        date: `${currentYear + 1}-01-01 00:00:00`,
         image: 'new-year.jpg'
     }
 }
@@ -76,7 +76,7 @@ class Countdown {
             date: document.querySelector('.holiday__date'),
             image: document.querySelector('.image')
         }
-        this.numbers = {
+        this.clockNumbers = {
             days: document.getElementById('days'),
             hours: document.getElementById('hours'),
             minutes: document.getElementById('minutes'),
@@ -92,19 +92,18 @@ class Countdown {
     }
 
     showCountdown() {
-        const today = new Date();
-        const currentUTCTime = today.getTime();
-        const gap = this.expectedDate - currentUTCTime;
+        const today = new Date().getTime();
+        const gap = this.expectedDate - today;
 
-        const seconds = Math.floor((gap / 1000) % 60);
-        const minutes = Math.floor((gap / 1000 / 60) % 60);
-        const hours = Math.floor((gap / (1000 * 60 * 60)) % 24);
         const days = Math.floor(gap / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((gap % (1000 * 60)) / 1000);
 
-        this.numbers.days.innerText = String(days);
-        this.numbers.hours.innerText = String(hours);
-        this.numbers.minutes.innerHTML = `<span>${('0' + minutes).slice(-2, -1)}</span><span>${('0' + minutes).slice(-1)}</span>`;
-        this.numbers.seconds.innerHTML = `<span>${('0' + seconds).slice(-2, -1)}</span><span>${('0' + seconds).slice(-1)}</span>`;
+        this.clockNumbers.days.innerText = String(days);
+        this.clockNumbers.hours.innerText = String(hours);
+        this.clockNumbers.minutes.innerHTML = `<span>${('0' + minutes).slice(-2, -1)}</span><span>${('0' + minutes).slice(-1)}</span>`;
+        this.clockNumbers.seconds.innerHTML = `<span>${('0' + seconds).slice(-2, -1)}</span><span>${('0' + seconds).slice(-1)}</span>`;
     }
 
     init () {
@@ -114,6 +113,7 @@ class Countdown {
         }, 1000);
     }
 }
+
 
 const countdownOfClosestHoliday = new Countdown(closestHoliday.name, closestHoliday.dateTitle, closestHoliday.date, closestHoliday.image);
 countdownOfClosestHoliday.init();
